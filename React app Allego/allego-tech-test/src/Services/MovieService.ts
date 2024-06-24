c611c5a0bbd5919fbeee7429d98efe4adb9c6e17
AndrewSA123 <andrewknott_123@hotmail.co.uk>
2024-06-24 13:21:42 +0100

    did a stupid

--- React app Allego/allego-tech-test/src/Services/MovieService.ts
+++ React app Allego/allego-tech-test/src/Services/MovieService.ts
@@ -1,10 +1,13 @@
 import axios from 'axios';
 import {Root, Movie} from '../ViewModels/Models.ts';
 
+
 const base_url : string = "https://www.omdbapi.com/?";
+var apiKey : string | undefined = process.env.REACT_APP_MOVIE_API_KEY;
+console.log(process.env.REACT_APP_MOVIE_API_KEY);
 
 export const fetchSearchResults = async (searchText : string, page : number) : Promise<Root> => {
-    const response: Root = (await axios.get(`${base_url}s=${searchText}&page=${page}&apikey=5f41a62d`)).data
+    const response: Root = (await axios.get(`${base_url}s=${searchText}&page=${page}&apikey=${apiKey}`)).data
 
     return new Promise((res, rej) => {
         return res(response);
@@ -12,7 +15,7 @@ export const fetchSearchResults = async (searchText : string, page : number) : P
 }
 
 export const getSpecificMovieData = async (imdbID : string) : Promise<Movie> => {
-    const response: Movie = (await axios.get(`${base_url}i=${imdbID}&apikey=5f41a62d`)).data
+    const response: Movie = (await axios.get(`${base_url}i=${imdbID}&apikey=${apiKey}`)).data
 
     return new Promise((res, rej) => {
         return res(response);
